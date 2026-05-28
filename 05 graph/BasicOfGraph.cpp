09f43743f99c0e5a377b844f62f8bd3798075c49
prince <legcy143@gmail.com>
2026-05-28 21:08:55 +0530

    feat: add initial graph class implementation

---
+++ 05 graph/BasicOfGraph.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph {
+    int v;
+    
+};
+
+
+int main(){
+    std::cout<<"har har mahadev ~Graph \n";
+
+
+    return 0;
+}
